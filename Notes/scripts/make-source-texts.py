38550d50caf5f0bf0c40850e7f19db26604c906a
Dmytro Kyrychuk <dmytro@kyrych.uk>
2020-03-15 13:22:23 +0200

    Add source making script

---
+++ Notes/scripts/make-source-texts.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import argparse
+import os
+import os.path
+import tempfile
+import urllib.request
+import xml.sax
+import zipfile
+
+from lxml import etree
+
+
+def main(args):
+    """Copy text from xml comments into tags."""
+    translations_root = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+
+    for dirpath, dirnames, filenames in os.walk(translations_root):
+        for file_name in filenames:
+            if not file_name.endswith(".xml"):
+                continue
+            file_path = os.path.join(dirpath, file_name)
+            transform_file(file_path)
+
+
+def transform_file(file_path):
+    with open(file_path, "rb") as inf:
+        doc = etree.parse(inf)
+
+    # Replace tag text with last comment text
+    last_comment = None
+    for el in doc.iter():
+        if isinstance(el, etree._Comment) and el.text.startswith(" EN: "):
+            last_comment = el.text[len(" EN: ") :].strip()
+        else:
+            if last_comment is not None:
+                el.text = last_comment
+            last_comment = None
+
+    with open(file_path, "wb") as outf:
+        outf.write(etree.tostring(doc, encoding="UTF-8", xml_declaration=True))
+
+
+if __name__ == "__main__":
+    import sys
+
+    main(sys.argv[1:])
